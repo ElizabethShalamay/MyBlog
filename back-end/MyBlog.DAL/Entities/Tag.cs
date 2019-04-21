5a48ee2777b51e47eaa35f76f068d05cdd6f5ee4
Yelyzaveta Shalamai <yelyzaveta_shalamai@epam.com>
2019-04-21 15:12:36 +0300

    fix tag name migration

--- back-end/MyBlog.DAL/Entities/Tag.cs
+++ back-end/MyBlog.DAL/Entities/Tag.cs
@@ -1,11 +1,16 @@
 ﻿using DAL.Entities;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyBlog.DAL.Entities
 {
     public class Tag : IIdentical
     {
         public int Id { get; set; }
+        [Column(TypeName = "VARCHAR")]
+        [StringLength(450)]
+        [Index]
         public string Name { get; set; }
         public virtual IList<Post> Posts { get; set; } 
     }
---
+++ back-end/MyBlog.DAL/Infrastructure/DalModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using MyBlog.DAL.Entities;
+using MyBlog.DAL.Interfaces;
+using MyBlog.DAL.Repository;
+
+namespace DAL.Infrastructure
+{
+    public class DalModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Repository<Comment>>().As<IRepository<Comment>>().InstancePerRequest();
+            builder.RegisterType<Repository<Post>>().As<IRepository<Post>>().InstancePerRequest();
+            builder.RegisterType<Repository<Tag>>().As<IRepository<Tag>>().InstancePerRequest();
+
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<UnitOfWork>().As<IIdentityManager>().InstancePerRequest();
+        }
+    }
+}
